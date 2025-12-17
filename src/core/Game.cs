6e8cb5370f1195295343b9ffde270b571d5dadbd
shysolocup <88659700+shysolocup@users.noreply.github.com>
2025-12-17 03:50:17 -0600

    guh

--- src/core/Game.cs
+++ src/core/Game.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 [Tool]
 [GlobalClass, Icon("uid://boo8iw5pvoaa8")]
@@ -7,6 +6,16 @@ public partial class Game : Node
 {
 
 	public static Game Instance { get; private set; }
+	
+	public static Node GetGameNode(NodePath nodePath)
+	{
+		return Instance.GetNode(nodePath);
+	}
+
+	public static T GetGameNode<T>(NodePath nodePath) where T : Node
+	{
+		return Instance.GetNode<T>(nodePath);
+	}
 
 	public override void _Notification(int what)
 	{
